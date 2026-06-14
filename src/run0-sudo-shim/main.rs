2d95937cd713b2789465d9dfa89b82161a9710c5
Grimmauld <Grimmauld@grimmauld.de>
2026-06-14 22:00:04 +0200

    add multicall match

--- src/run0-sudo-shim/main.rs
+++ src/run0-sudo-shim/main.rs
@@ -10,18 +10,30 @@ mod sudo;
 use crate::common::*;
 
 fn main() {
-    let cli = sudo::Cli::parse();
-
     let cwd = env::current_dir()
         .map(|p| p.to_string_lossy().into_owned())
         .ok();
 
     let env = env::vars().map(|(key, _)| key).collect();
 
-    let mut cli = ShimResult::finalize(
-        sudo::parse_to_run0_cli(cli, cwd, get_current_uid(), env),
-        env!("CARGO_PKG_NAME"),
-    )
+    let cmd = env::args_os()
+        .next()
+        .and_then(|arg0| {
+            std::path::Path::new(&arg0)
+                .file_name()
+                .and_then(|s| s.to_str())
+                .map(|s| s.to_string())
+        })
+        .unwrap_or_else(|| die("failed to read argv[0]"));
+
+    #[allow(clippy::match_single_binding)]
+    let mut cli = match cmd.as_str() {
+        // "pkexec" => panic!("not implemented"),
+        _ => ShimResult::finalize(
+            sudo::parse_to_run0_cli(sudo::Cli::parse(), cwd, get_current_uid(), env),
+            "sudo",
+        ),
+    }
     .into_iter();
 
     let program = cli.next().unwrap_or_else(|| die("unable to construct cli"));
